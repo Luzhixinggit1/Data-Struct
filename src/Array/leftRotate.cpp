d31e6c8905de36d6e1ae57c405bb1d8df69890dd
Luzhixinggit1 <1726361003@qq.com>
2017-12-06 22:17:05 +0800

    Update leftRotate.cpp

--- src/Array/leftRotate.cpp
+++ src/Array/leftRotate.cpp
@@ -65,8 +65,70 @@ void leftRotateOneByOne(int arr[], int n)
 	arr[n - 1] = temp;
 }
 
-void leftRotate0(int arr[], int d, int n)
+void leftRotate(int arr[], int d, int n)
 {
 	for (int i = 0; i < d; i++)
 		leftRotateOneByOne(arr, n);
 }
+
+//三：方法三
+/*
+次为方法二的扩展，找到d与n的最大公约数，相当于把每一个数组分成gcd(最大公约数)个集合，每个集合都进行obebyone的移动
+
+*/
+int gcd(int a, int b)
+{
+	if (b == 0)
+		return a;
+	else
+		return gcd(b, a%b);
+}
+
+void leftRotate(int arr[], int d, int n)
+{
+	int i, j, k, temp;
+	for (i = 0; i < gcd(n, d); i++)
+	{
+		temp = arr[i];
+		j = i;
+		while (1)
+		{
+			k = j + d;
+			if (k >= n)
+				k = k - n;
+			if (k == i)break;
+			arr[j] = arr[k];
+			j = k;
+		}
+		arr[j] = temp;
+	}
+}
+
+//四：方法四：
+
+/*
+此方法相当于把数组分成a[0]-a[d-1]和a[d]-a[n-1],分别将这两个部分反转，最后将整个数组反转
+*/
+
+void rvereseArray(int arr[], int start, int end)
+{
+	int temp;
+	while (start < end)
+	{
+		temp = arr[start];
+		arr[start] = arr[end];
+		arr[end] = temp;
+		start++;
+		end--;
+	}
+}
+
+void leftRotatef(int arr[], int d, int n)
+{
+	rvereseArray(arr,0, d - 1);
+	rvereseArray(arr,d, n - 1);
+	rvereseArray(arr,0, n - 1);
+}
+
+
+
